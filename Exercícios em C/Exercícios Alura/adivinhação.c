c3c2c24a16e7b8cbda7c46d1b5283ba48bab3115
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-03-21 09:02:31 -0300

    Creiei jogo de adivinhação

---
+++ Exercícios em C/Exercícios Alura/adivinhação.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    printf("**************************************\n");
+    printf("BEM VINDO AO JOGO DE ADIVINHAÇÃO!!\n");
+    printf("**************************************\n");
+
+    int secreto= 42;
+    int chute;
+
+    printf("Chute um número: \n");
+    scanf("%d",&chute);
+    printf("Você chutou %d ", chute)
+
+}
