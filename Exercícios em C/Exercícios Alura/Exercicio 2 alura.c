760a07e4da9923aaba61de096232d7ba5967eb6e
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-03-20 21:25:13 -0300

    realizei exercicios

---
+++ Exercícios em C/Exercícios Alura/Exercicio 2 alura.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    printf("Digite o primeiro valor: ");
+    scanf("%d",&x);
+    printf("Digite outro Valor: ");
+    scanf("%d",&y);
+    printf("A multiplicação de ambos é %d",x*y);
+}
