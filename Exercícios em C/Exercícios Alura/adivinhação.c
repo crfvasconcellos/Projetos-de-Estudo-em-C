f68ebd7b82772d902275c12d868b3319964fb69f
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-03-24 09:36:42 -0300

    melhorei o código

--- Exercícios em C/Exercícios Alura/adivinhação.c
+++ Exercícios em C/Exercícios Alura/adivinhação.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define TENTATIVAS 5
+
 int main(){
     printf("**************************************\n");
     printf("BEM VINDO AO JOGO DE ADIVINHAÇÃO!!\n");
@@ -7,12 +9,19 @@ int main(){
 
     int secreto= 42;
     int chute;
-for (int i = 1; i <= 3; i= i +1)
+
+for (int i = 1; i <= TENTATIVAS; i= i +1)
 {
 
     printf("Chute um número: \n");
     scanf("%d",&chute);
     printf("Você chutou %d \n", chute);
+
+    if (chute<0) {
+        printf("VOCÊ NÃO PODE CHUTAR NÚMEROS NEGATIVOS!!\n");
+        i= i -1;
+        continue;
+    }
 
     if (chute==secreto){
        
@@ -31,7 +40,7 @@ for (int i = 1; i <= 3; i= i +1)
         }
         
         printf("Você errou!! Tente novamente!\n");
-        printf("TENTATIVA %d DE 3\n", i);
+        printf("TENTATIVA %d DE %d\n", i,TENTATIVAS);
     }
 
 }
