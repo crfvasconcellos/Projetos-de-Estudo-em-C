4577c289e3e6c2680dd8e2227c5c5f21a7272c16
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-03-29 20:48:05 -0300

    fiz quest 10 da att 4 de LP

--- Exercícios em C/Atividades UFT/Atividade 4/att 4 quest 10.c
+++ Exercícios em C/Atividades UFT/Atividade 4/att 4 quest 10.c
@@ -6,5 +6,57 @@
 
 int main() {
 
+    int tipo,m,deco,val,est,net;
+
+    printf("Quais são os m2 do seu projeto? ");
+    scanf("%d",&m);
+
+    printf("Qual Tipo de Projeto Você quer? \n [1] Projeto Básico \n [2] Projeto Complementar?\n");
+    scanf("%d",&tipo);
+
+    if (tipo==1)
+    {
+        val= m*20;
+    }
+    else if (tipo==2)
+    {
+        val= m*30;
+    }
+    
+    else{
+        printf("Valor inválido!");
+        return 0; 
+    }
+
+    printf("Deseja adicionar o serviço de decoração? \n [1] SIM \n [2] NÃO \n");
+    scanf("%d",&deco);
+
+
+    if (deco==1)
+    {
+        int comodos;
+        printf("Quantos cômodos deseja decorar? \n");
+        scanf("%d",&comodos);
+        val=val+comodos*100;
+    }
+
+    printf("Deseja um projeto estruturado? \n [1] SIM \n [2] NÃO \n");
+    scanf("%d",&est);
+
+    if(est==1){
+        val=val+1200;
+    }
+
+    printf("Deseja realizar um cadastro pela internet? \n [1] SIM \n [2] NÃO \n");
+    scanf("%d",&net);
+
+    if (net==1)
+    {
+        printf("Você Recebeu um desconto de 5%%! \n");
+        val= val-val*0.95;
+    }
+    
+
+    printf("O valor total do seu projeto é %d",val);
     
 }
