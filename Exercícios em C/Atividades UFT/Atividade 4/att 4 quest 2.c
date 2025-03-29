838848a4c3e1a19cf9695f82bc037d28d72ababe
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-03-29 18:50:27 -0300

    Fiz att 2 da lista de LP

---
+++ Exercícios em C/Atividades UFT/Atividade 4/att 4 quest 2.c
@@ -0,0 +1,32 @@
+/*2) Elabore um algoritmo que mostre o tipo de eleitor (obrigatório, opcional ou não eleitor) de acordo com sua idade.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int idade;
+
+    printf("Qual a sua idade? ");
+    scanf("%d",&idade);
+
+    if (idade<16 && idade>0){
+        printf("Você não pode ser um eleitor!");
+
+    }
+    else if (idade>=16 && idade<18)
+    {
+        printf("Você é um eleitor Opicional!");
+    }
+    else if (idade>=18)
+    {
+        printf("Você é um eleitor Obrigatório!");
+    }
+    else if (idade<0)
+    {
+        printf("Essa não é um opção Válida.");
+        return 1;
+    }
+    
+    return 0;
+
+}
