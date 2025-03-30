290041d981bd6c4b808b9eb560f0ef0a20097e9f
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-03-30 17:51:26 -0300

    adicionei níveis

--- Exercícios em C/Exercícios Alura/adivinhação.c
+++ Exercícios em C/Exercícios Alura/adivinhação.c
@@ -9,9 +9,13 @@ int main(){
     printf("BEM VINDO AO JOGO DE ADIVINHAÇÃO!!\n");
     printf("**************************************\n");
 
+    int nivel,nt,pts=0; //conta os níveis de desafio e mostra os pontos só se ganhar
     int x = time(0);
     srand(x);
     int aleatorio= rand();
+
+    printf("Deseja jogar no nível: \n [1] Fácil \n [2] Médio \n [3] Díficil \n R: ");
+    scanf("%d",&nivel);
     
     
     int secreto= aleatorio % 100;
@@ -22,6 +26,40 @@ int main(){
     
 
 while (ganhou==0){
+
+    if (nivel==1)
+    {
+        nt=20;
+        if (i>=21)
+        {
+            printf("Você Ultrapassou seu número de Tentativas! \n JOGO ENCERRADO!");
+            break;
+        }
+        
+    }
+
+    else if (nivel==2)
+    {
+        nt=10;
+        if (i==11)
+        {
+            printf("Você Ultrapassou seu número de Tentativas! \n JOGO ENCERRADO!");
+            break;
+        }
+        
+    }
+
+    else if (nivel==3)
+    {
+        nt=5;
+        if (i==6)
+        {
+            printf("Você Ultrapassou seu número de Tentativas! \n");
+            break;
+        }
+        
+    }
+
     i= i + 1;
     printf("Chute um número: \n");
     scanf("%d",&chute);
@@ -36,6 +74,8 @@ while (ganhou==0){
     if (chute==secreto){
        
         printf("PARABÉNS, VOCÊ ACERTOU!!! (O NÚMERO SECRETO ERA %d)\n",secreto);
+        pts=1;
+
         break;
 
     }
@@ -53,12 +93,16 @@ while (ganhou==0){
         }
         
         printf("Você errou!! Tente novamente!\n");
-        printf("TENTATIVA %d !\n", i);
+        printf("TENTATIVA %d DE %d !\n", i,nt);
         pontuacao= pontuacao - (diferenca)/2;
     }
 
 }
 printf("JOGO ENCERRADO!\n");
 printf("VOCÊ TERMINOU COM %d TENTATIVAS! \n",i);
+if (pts==0)
+{
+    pontuacao=0;
+}
 printf("VOCÊ OBTEVE A PONTUAÇÃO %.1f DE 1000 !", pontuacao);
 }
