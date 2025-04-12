54473a90aadc8fb6daad00b03326dbfc2eee6029
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-04-12 14:46:08 -0300

    fiz a forca

--- Exercícios em C/Exercícios Alura/Módulo 2 alura C/forca.c
+++ Exercícios em C/Exercícios Alura/Módulo 2 alura C/forca.c
@@ -1,10 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
     char palavra_secreta[20];
-
     sprintf(palavra_secreta,"MELANCIA");
-    printf("%s",palavra_secreta);
+
+
+
+    int acertou=0;
+    int enforcou=0;
+
+    char chutes[26];
+    int tentativas=0;
+
+    do
+    {
+        for (size_t i = 0; i < strlen(palavra_secreta); i++) //aparecer _ da palavra secreta
+        {
+            int achou=0;
+
+            for (int j = 0; j < tentativas; j++)
+            {
+                
+                if (chutes[j]==palavra_secreta[i])
+                {
+                    achou=1;
+                    printf("\n SEU CHUTE FOI CORRETO !! \n ");
+                    break;
+                }
+            }
+            if (achou==1)
+            {
+                printf("%c",palavra_secreta[i]);
+            }
+            else
+            {
+                printf("_ ");
+            }
+            
+            
+           
+        }
+        printf("\n");
+        
+        char chute;
+        printf("\nQual o seu chute? ");
+        scanf(" %c",&chute);
+
+        chutes[tentativas]=chute;
+        tentativas++;   
+
+       
+
+        
+    } while (!acertou && !enforcou);
+    
 
 }
