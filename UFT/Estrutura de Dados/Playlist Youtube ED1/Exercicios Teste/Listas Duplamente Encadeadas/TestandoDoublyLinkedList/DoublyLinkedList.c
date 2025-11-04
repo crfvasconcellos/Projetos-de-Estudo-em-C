342053a29b17fea4b6fc58b6988db3823d47e383
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-11-04 17:54:15 -0300

    implementei o destroy e o addFirst

---
+++ UFT/Estrutura de Dados/Playlist Youtube ED1/Exercicios Teste/Listas Duplamente Encadeadas/TestandoDoublyLinkedList/DoublyLinkedList.c
@@ -0,0 +1,102 @@
+#include "DoublyLinkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct _doubly_node 
+{
+    int val;
+    struct _doubly_node* prev;
+    struct _doubly_node* after;
+
+}DoublyNode,Node;
+
+typedef struct _doubly_linked_list 
+{
+    Node * begin;
+    Node* end;
+    size_t size;
+
+}DoublyLinkedList,List;
+
+
+Node * Node_create(int val){
+
+    Node * node = (Node*) calloc(1,sizeof(Node));
+    node->val= val;
+    node->after= NULL;
+    node->prev=NULL;
+    return node; 
+
+}
+
+
+DoublyLinkedList * DoublyLinkedList_create()
+{
+    DoublyLinkedList* list = (DoublyLinkedList*) calloc(1,sizeof(DoublyLinkedList));
+    list->begin= NULL;
+    list->end= NULL;
+    list->size= 0;
+
+    return list;
+
+}
+
+void DoublyLinkedList_destroy(DoublyLinkedList** L){
+
+    Node * aux = (*L)->begin;
+    
+
+    while (aux != NULL)
+    {
+        Node * next = aux->after;
+        free(aux);
+        aux = next;
+        
+    }
+
+    free(*L);
+    *L = NULL;
+
+}
+
+
+
+bool DoublyLinkedList_isempety(DoublyLinkedList * L){
+
+    if (L->size == 0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+
+
+}
+
+
+void DoublyLinkedList_addfirst(DoublyLinkedList * L, int val){
+
+    Node  * newNode = Node_create(val);
+
+    if (DoublyLinkedList_isempety(L))
+    {
+      
+        L->end = newNode;
+        L->begin = newNode;
+
+    }
+    else
+    {
+        newNode->after = L->begin;
+        L->begin->prev = newNode;
+        L->begin = newNode;
+        
+    }
+    
+
+    L->size++;
+
+}
+
---
+++ UFT/Estrutura de Dados/Playlist Youtube ED1/Exercicios Teste/Listas Duplamente Encadeadas/TestandoDoublyLinkedList/DoublyLinkedList.h
@@ -0,0 +1,18 @@
+#ifndef DOUBLYLINKEDLIST_H // DoublyLinkedList
+#define DOUBLYLINKEDLIST_H
+
+#include <stdbool.h>
+
+typedef struct _doubly_node DoublyNode,Node;
+typedef struct _doubly_linked_list DoublyLinkedList, List;
+
+Node * Node_create();
+
+DoublyLinkedList * DoublyLinkedList_create();
+
+bool DoublyLinkedList_isempety(DoublyLinkedList * L);
+
+void DoublyLinkedList_destroy(DoublyLinkedList* L);
+void DoublyLinkedList_addfirst(DoublyLinkedList * L, int val);
+
+#endif
